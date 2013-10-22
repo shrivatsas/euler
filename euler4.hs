802f576265071a48f722ff415a30ee8d023268af
shrivatsa <shrivatsa.swadi@gmail.com>
2013-10-22 09:58:19 +0530

    sunday - 20Oct

---
+++ euler4.hs
@@ -0,0 +1,5 @@
+-- A palindromic number reads the same both ways. 
+-- The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+-- Find the largest palindrome made from the product of two 3-digit numbers.
+
+main = print ( maximum [ x | y <- [100 .. 999], z <- [y .. 999], let x = y*z, let s = show x, s == reverse s ])
